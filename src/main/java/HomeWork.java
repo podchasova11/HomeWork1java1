cb0daba19eba385e217fa7b120255ce00fddabfc
Mila <Pod4asova20@gmail.com>
2022-04-07 09:44:36 +0300

    Initial commit

---
+++ src/main/java/HomeWork.java
@@ -0,0 +1,48 @@
+public class HomeWork {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 3, b = 5, sum;
+        sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        }
+        if (sum < 0) {
+            System.out.println("Сумма Отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 10;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        }
+        if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 10, b = 14;
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        if (a <= b) {
+            System.out.println("a <= b");
+        }
+    }
+}
